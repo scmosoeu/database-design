73e591e35f992bb88d7e22f453b526ac03906f5e
Selebogo Mosoeu <scmosoeu@gmail.com>
2024-03-18 21:00:10 +0200

    created tables

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dbconnector import load_config
 
+from models import ProcessDate, SalesSummary, db
 from utils import SQLConnector
 # ORDER OF TASKS
 
@@ -11,11 +12,21 @@ sql_connector = SQLConnector(config)
 
 # Extract data
 # df = sql_connector.run_query_li("SELECT TOP 5 * FROM daily_prices")
-df = sql_connector.run_query_li(
-    "SELECT table_name FROM INFORMATION_SCHEMA.TABLES")
+table_names_tuple = sql_connector.run_query_li(
+    "SELECT table_name FROM INFORMATION_SCHEMA.TABLES"
+)
 
-for tname in df:
-    print(tname[0])
-# print(df.fetchall())
+# table_names = [tname[0] for tname in table_names_tuple]
+# if 'process_date' not in table_names:
+#     extract_dates = sql_connector.run_query_li(
+#         "SELECT * FROM extract_date ORDER BY information_date ASC"
+
+#     for extract_date in extract_dates:
+#         process_date = ProcessDate(
+#             information_date=extract_date[0]
+#         )
+#         db.session.add(process_date)
+
+#     db.session.commit()
 
 sql_connector.close_all()
--- models.py
+++ models.py
@@ -14,6 +14,45 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Product(db.Model):
+class ProcessDate(db.Model):
+    __tablename__ = 'process_date'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    information_date = db.Column(db.String(20))
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    commodity = db.Column(db.String(50))
+    product_sales = db.relationship(
+        'ProductSales', backref='products', lazy=True)
+
+
+class Container(db.Model):
+    __tablename__ = 'containers'
+    id = db.Column(db.Integer, primary_key=True)
+    container = db.Column(db.String(50))
+    container_sales = db.relationship(
+        'ProductSales', backref='containers', lazy=True)
+
+
+class ProductCombination(db.Model):
+    __tablename__ = 'combinations'
+    id = db.Column(db.Integer, primary_key=True)
+    combination = db.Column(db.String(50))
+    combination_sales = db.relationship(
+        'ProductSales', backref='combinations', lazy=True)
+
+
+class ProductSales(db.Model):
+    __tablename__ = 'product_sales'
+    information_date = db.Column(db.String(20))
+    commodity_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    container_id = db.Column(db.Integer, db.ForeignKey('containers.id'))
+    combination_id = db.Column(db.Integer, db.ForeignKey('combinations.id'))
+    total_value_sold = db.Column(db.Float)
+    total_quantity_sold = db.Column(db.Integer)
+    total_kg_sold = db.Column(db.Float)
+
+
+db.create_all()
